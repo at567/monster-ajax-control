2eb01832f3b4dff83a87a9c8726155207298adb7
unknown <you@example.com>
2020-06-30 17:54:20 -0700

    Updates

---
+++ precache-manifest.29dd63f7d189a3ab28ff541f89f6876f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7296e2a5cb7faf0791cb6da1abab34",
+    "url": "/monster-ajax-control/index.html"
+  },
+  {
+    "revision": "e5c3d68bcbb1708f22dd",
+    "url": "/monster-ajax-control/static/css/main.264af142.chunk.css"
+  },
+  {
+    "revision": "60c5862aa0c68dca5096",
+    "url": "/monster-ajax-control/static/js/2.93f389f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-ajax-control/static/js/2.93f389f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c3d68bcbb1708f22dd",
+    "url": "/monster-ajax-control/static/js/main.dbf4a195.chunk.js"
+  },
+  {
+    "revision": "adda71e931628629ef48",
+    "url": "/monster-ajax-control/static/js/runtime-main.6b12313d.js"
+  }
+]);
